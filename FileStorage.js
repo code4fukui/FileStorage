846bbd3c838f2113928c041a459e229a0c8dae4c
Taisuke Fukuno <fukuno@jig.jp>
2025-12-11 09:43:29 +0900

    use temp dir for test

--- FileStorage.js
+++ FileStorage.js
@@ -4,7 +4,9 @@ export class FileStorage {
     this.basedir = basedir;
   }
   async save(path, bin) {
-    if (path.indexOf("..") >= 0 || path.indexOf("//") >= 0 || path[0] == "/") throw new Error("illegal path");
+    if (path.indexOf("..") >= 0 || path.indexOf("//") >= 0 || path[0] == "/") {
+      throw new Error("illegal path");
+    }
     const n = path.lastIndexOf("/");
     if (n >= 0) {
       const dir = path.substring(0, n);
--- FileStorage.test.js
+++ FileStorage.test.js
@@ -3,16 +3,16 @@ import { FileStorage } from "./FileStorage.js";
 
 Deno.test("simple", async () => {
   const bin = new TextEncoder().encode("abc");
-  const fs = new FileStorage("data");
-  const path = "data/test/20251211/abc.txt";
+  const fs = new FileStorage("temp");
+  const path = "test/20251211/abc.txt";
   await fs.save(path, bin);
   const bin2 = await fs.load(path);
   t.assertEquals(bin, bin2);
 });
 Deno.test("illegalpath", async () => {
   const bin = new TextEncoder().encode("abc");
-  const fs = new FileStorage("data");
-  t.assertRejects(async () => await fs.save("/abc", bin));
-  t.assertRejects(async () => await fs.save("../abc", bin));
-  t.assertRejects(async () => await fs.save("//abc", bin));
+  const fs = new FileStorage("temp");
+  await t.assertRejects(async () => await fs.save("/abc", bin));
+  await t.assertRejects(async () => await fs.save("../abc", bin));
+  await t.assertRejects(async () => await fs.save("//abc", bin));
 });
